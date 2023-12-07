b59fb396f57d69edf0de3a43c83b2a6825b724cb
alexkobal <alexkobal90@gmail.com>
2023-12-07 17:05:02 +0100

    Added documentation in redme

--- docker_custom/go_docker.sh
+++ docker_custom/go_docker.sh
@@ -5,7 +5,7 @@ if [ "$(docker ps -q -a -f name=magic123)" ]; then
     if [ "$(docker ps -q -f name=magic123)" ]; then
         echo "Container exists and is running, no op"
     else
-        echo "Container exitsts but is not running, start it"
+        echo "Container exists but is not running, start it"
         docker start magic123
     fi
     echo "Container exists and running, exec it"
@@ -14,9 +14,10 @@ else
     # Container does not exist, run it
     echo "Container does not exist, run it"
     docker run -it \
+        --runtime=nvidia \
         --gpus all \
         --name magic123 \
         -w /workdir/Magic123 \
-        -v /data:/data\
-        magic123:latest
+        -v "$1":/data \
+        magic123:local
 fi
